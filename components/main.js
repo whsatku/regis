a2f0f4abb943814ce7e8cdff39be70f344359a50
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-04-28 08:27:48 +0700

    Save data to localforage

--- components/main.js
+++ components/main.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import DocumentTitle from 'react-document-title';
 
+import state from './state';
 import App from './app';
 import Login from './login';
 import UserInfo from './userinfo';
@@ -10,17 +11,19 @@ import Registration from './registration';
 import CourseSearch from './coursesearch';
 import CourseInfo from './courseinfo';
 
-ReactDOM.render((
-	<DocumentTitle title="Course Registration">
-		<Router history={hashHistory}>
-			<Route path="/" component={App}>
-				<IndexRoute component={UserInfo} />
-				<Route path="login" component={Login} />
-				<Route path="registration" component={Registration}>
-					<IndexRoute component={CourseSearch} />
-					<Route path="*" component={CourseInfo} />
+state.ready.then(() => {
+	ReactDOM.render((
+		<DocumentTitle title="Course Registration">
+			<Router history={hashHistory}>
+				<Route path="/" component={App}>
+					<IndexRoute component={UserInfo} />
+					<Route path="login" component={Login} />
+					<Route path="registration" component={Registration}>
+						<IndexRoute component={CourseSearch} />
+						<Route path="*" component={CourseInfo} />
+					</Route>
 				</Route>
-			</Route>
-		</Router>
-	</DocumentTitle>
-), document.getElementById('app'));
+			</Router>
+		</DocumentTitle>
+	), document.getElementById('app'));
+});
--- components/state.js
+++ components/state.js
@@ -1,5 +1,39 @@
-let state = {};
-state.user = '5610545757';
-state.enrolled = [];
+import localforage from 'localforage';
 
-export default state;
+class State{
+	_user = null;
+	_enrolled = [];
+
+	constructor(){
+		this.ready = this._loadData();
+	}
+
+	_loadData(){
+		let promises = [
+			localforage.getItem('user').then((data) => this._user = data),
+			localforage.getItem('enrolled').then((data) => this._enrolled = data),
+		];
+
+		return Promise.all(promises);
+	}
+
+	get user(){
+		return this._user;
+	}
+
+	set user(val){
+		this._user = val;
+		localforage.setItem('user', this._user);
+	}
+
+	get enrolled(){
+		return this._enrolled;
+	}
+
+	set enrolled(val){
+		this._enrolled = val;
+		localforage.setItem('enrolled', this._enrolled);
+	}
+}
+
+export default new State();
